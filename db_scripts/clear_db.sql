0e0143b93f77cb05f5d36be5e6c4558f5d3dac56
Martin Bauer <eashibby@gmx.de>
2025-02-21 12:49:37 +0100

    DBScript for db cleanup

---
+++ db_scripts/clear_db.sql
@@ -0,0 +1,5 @@
+DROP table if exists GYM_BACKEND.user_team;
+DROP table if exists GYM_BACKEND.users;
+DROP table if exists GYM_BACKEND.seasons;
+DROP table if exists GYM_BACKEND.matches;
+DROP table if exists GYM_BACKEND.teams;
