811d032add7d7c9298b61f74e95df4d086752c4a
Marcel Moll <marcel@watson-holmes.com>
2025-03-05 17:33:40 +0100

    adjusting matches to refer to a season and a playday

--- src/database/model/DBMatch.py
+++ src/database/model/DBMatch.py
@@ -9,10 +9,13 @@ class DBMatch(DBModel):
     id = Column(Integer, Sequence(f'{__name__.lower()}_id_seq'), primary_key=True)
     team1_id = Column(Integer, ForeignKey('teams.id'))
     team2_id = Column(Integer, ForeignKey('teams.id'))
+    season_id = Column(Integer, ForeignKey('seasons.id'))
+    playday = Column(Integer)
     score = Column(String(20))
 
     team1 = relationship("DBTeam", foreign_keys=[team1_id])
     team2 = relationship("DBTeam", foreign_keys=[team2_id])
+    season = relationship("DBSeason", foreign_keys=[season_id])
 
     def to_dict(self):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
--- src/dtos/match_dto.py
+++ src/dtos/match_dto.py
@@ -7,6 +7,9 @@ class MatchDTO:
         self.team1 = data.get('team1')
         self.team2_id = data.get('team2_id')
         self.team2 = data.get('team2')
+        self.season_id = data.get('season_id')
+        self.season = data.get('season')
+        self.playday = data.get('playday')
         self.score = data.get('score')
 
     def to_dict(self):
@@ -16,6 +19,9 @@ class MatchDTO:
             'team1':self.team1,
             'team2_id': self.team2_id,
             'team2':self.team2,
+            'season_id': self.season_id,
+            'season': self.season,
+            'playday': self.playday,
             'score': self.score
         }
     
@@ -23,6 +29,8 @@ class MatchDTO:
         return {
             'team1_id': self.team1_id,
             'team2_id': self.team2_id,
+            'season_id': self.season_id,
+            'playday': self.playday,
             'score': self.score
         }
 
@@ -35,6 +43,9 @@ class MatchDTO:
                 'team1': match.team1.name if match.team1 else None,
                 'team2_id': match.team2_id,
                 'team2': match.team2.name if match.team2 else None,
+                'season_id': match.season_id,
+                'season': match.season.name if match.season else None,
+                'playday': match.playday,
                 'score': match.score
             }
         )
@@ -46,6 +57,8 @@ class MatchDTO:
             'properties': {
                 'team1_id': {'type': 'integer'},
                 'team2_id': {'type': 'integer'},
+                'season_id': {'type': 'integer'},
+                'playday': {'type': 'integer'},
                 'score' : {'type': 'string'}
             },
             'required': ['team1_id','team2_id', 'score']
