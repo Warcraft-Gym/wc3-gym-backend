94c2ece975cdca60e961cf965e43856978e8d899
Martin Bauer <eashibby@gmx.de>
2025-03-08 20:25:49 +0100

    Series: Add date_time and caster, split score int player1 and player2 score

--- src/database/model/DBSeries.py
+++ src/database/model/DBSeries.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from src.database.model.DBModel import DBModel
 
@@ -6,9 +6,12 @@ class DBSeries(DBModel):
     __tablename__ = 'series'
     id = Column(Integer, Sequence(f'{__name__.lower()}_id_seq'), primary_key=True)
     match_id = Column(Integer, ForeignKey('matches.id'))
+    date_time = Column(DateTime)
+    caster = Column(String(50))
     player1_id = Column(Integer, ForeignKey('users.id'))
     player2_id = Column(Integer, ForeignKey('users.id'))
-    score = Column(String(20))
+    player1_score = Column(Integer)
+    player2_score = Column(Integer)
 
     match = relationship("DBMatch", foreign_keys=[match_id])
     player1 = relationship("DBUser", foreign_keys=[player1_id])
