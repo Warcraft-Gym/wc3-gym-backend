ef75fa479a6ad3b85ebc103db34d759e3189d481
Martin Bauer <eashibby@gmx.de>
2025-03-08 20:26:17 +0100

    Import/Export: add function to import matches

--- src/api/import_export_api.py
+++ src/api/import_export_api.py
@@ -1,17 +1,22 @@
 import logging
 from app import app
+from datetime import datetime
 from flask import request, jsonify, send_file
 from flask_jwt_extended import jwt_required
 from custom_exceptions import NotFoundException
 from flasgger import swag_from
 from src.dtos.season_dto import SeasonDTO
+from src.dtos.user_dto import UserDTO
+from src.dtos.team_dto import TeamDTO
+from src.dtos.series_dto import SeriesDTO
+from src.dtos.match_dto import MatchDTO
 import pandas as pd
 import io
 from io import BytesIO
 import openpyxl
 from src.util.import_util import ImportUtil
-from src.dtos.user_dto import UserDTO
-from src.dtos.team_dto import TeamDTO
+
+
 from src.util.query_util import QueryUtil
 
 logger = logging.getLogger(__name__)
@@ -60,17 +65,16 @@ def import_season():
         file = request.files['file']
         if file.filename == '':
             return jsonify({"error": "No selected file"}), 400
+        player_name_id = {}
         if file and file.filename.endswith(('.xlsx', '.xls')):
             file_stream = io.BytesIO(file.read())
             
             # Load the Google Sheet into a DataFrame
-            df = pd.read_excel(file_stream, sheet_name=None)
-            players = df.get('Players')
+            df_players = pd.read_excel(file_stream, sheet_name='Players')
 
             teams = []
             teams_players = {}
-
-            for index, row in players.iterrows():
+            for index, row in df_players.iterrows():
                 user_data = {
                         'name': ImportUtil.isNa(row['Bnet (no ID)']),
                         'battleTag': ImportUtil.isNa(row['Bnet']),
@@ -90,7 +94,7 @@ def import_season():
                     user = app.user_app_service.create_user(UserDTO(user_data))
                 else:
                     user = app.user_app_service.update_user(users[0].id, UserDTO(user_data))
-                
+                player_name_id[user.name] = user.id
 
                 team_name = ImportUtil.isNa(row['Team Abbr'])
                 if team_name and not teams_players.get(team_name):
@@ -102,7 +106,7 @@ def import_season():
                 elif team_name:
                     players = teams_players.get(team_name)
                     players.append(user.id)
-
+                    
             season_data = {
                 'name' : season_name
             }
@@ -119,6 +123,7 @@ def import_season():
                     season_id = found_seasons[0].id
             
             team_ids = []
+            team_name_id = {}
             for team_data in teams:
                 query = QueryUtil.parseQuery("name == " +  team_data.get('name'))
                 if not query or not query.elementA:
@@ -129,14 +134,86 @@ def import_season():
                     team = app.team_app_service.create_team(TeamDTO(team_data))
                 else:
                     team = app.team_app_service.update_team(found_teams[0].id, TeamDTO(team_data))
+                team_name_id[team.name] = team.id
                 team_ids.append(team.id)
                 players = teams_players.get(team.name)
                 app.team_app_service.addPlayers(team.id, season_id, players)
 
             app.season_app_service.addTeams(season_id, team_ids)
             
-            
-            matches = df.get('Matches')
+            excel_file = pd.ExcelFile(file_stream)
+            sheet_names = excel_file.sheet_names
+            week_sheets = [name for name in sheet_names if name.lower().startswith("week")]
+
+            # Count the number of "Week" sheets
+            number_of_week_sheets = len(week_sheets)
+
+            for i in range(1,number_of_week_sheets+1):
+                week = pd.read_excel(file_stream, sheet_name=f"Week {i}", header=None)
+                matchups_rows = []
+                for index, row in week.iterrows():
+                    if "VS" in row.values:
+                        matchups_rows.append(index)
+                matchups_rows.append(len(week))
+                for start, end in zip(matchups_rows, matchups_rows[1:]): 
+                    team1 = None
+                    team2 = None
+                    match = None
+                    for index, row in week.iloc[start:end].iterrows():
+                        if index == start:
+                            team1 = ImportUtil.isNa(row[0])
+                            team2 = ImportUtil.isNa(row[2])
+                            if not team1 or not team2:
+                                raise Exception('Team 1 or Team 2 not properly identified in row:]')
+                            team1_id = team_name_id[team1]
+                            team2_id = team_name_id[team2]
+                            q_string = f"team1_id=={team1_id} and team2_id=={team2_id} and season_id=={season_id}"
+                            query = QueryUtil.parseQuery(q_string)
+                            if not query or not query.elementA:
+                                raise Exception(f"No valid query found: {q_string}")
+                            found_matches = app.match_app_service.search(query)
+                            if not found_matches:
+                                match_data = {
+                                    'team1_id': team1_id,
+                                    'team2_id': team2_id,
+                                    'season_id': season_id,
+                                    'playday': i
+                                }
+                                match = app.match_app_service.create_match(MatchDTO(match_data))
+                            else:
+                                match = found_matches[0]
+                            continue
+                        if "Date" in row.values:
+                            continue
+                        if pd.isnull(row[5]) or pd.isnull(row[8]):
+                            continue
+                        if not team1 or not team2:
+                            raise Exception(f"Teams could not be identified for Week {i} and row range {start}/{end}")
+                        
+                        player1_id = player_name_id[row[5].rstrip("*")]
+                        player2_id = player_name_id[row[8].rstrip("*")]
+                        date_time = None
+                        if ImportUtil.isNa(row[4]) and ImportUtil.isNa(row[3]):
+                            date_time = datetime.combine(row[4], row[3])
+                        series_data = {
+                            'caster': ImportUtil.isNa(row[1]),
+                            'date_time': date_time,
+                            'match_id' : match.id,
+                            'player1_id': player1_id,
+                            'player1_score': ImportUtil.isNa(row[6]),
+                            'player2_score': ImportUtil.isNa(row[7]),
+                            'player2_id': player2_id
+                        }
+                        series_q_string = f"match_id=={match.id} and player1_id=={player1_id} and player2_id=={player2_id}"
+                        series_query = QueryUtil.parseQuery(series_q_string)
+                        if not query or not query.elementA:
+                            raise Exception(f"No valid query found: {series_q_string}")
+                        found_series = app.series_app_service.search(series_query)
+                        if not found_series:
+                            series = app.series_app_service.create_series(SeriesDTO(series_data))
+                        else:
+                            series = found_series[0]
+                            series = app.series_app_service.update_series(series.id, SeriesDTO(series_data))                       
 
             
             return jsonify({"message": "File uploaded successfully and data inserted into database"}), 200
