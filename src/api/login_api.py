03fb8cf8557288a54dbd019291454736e81e126d
Martin Bauer <eashibby@gmx.de>
2025-04-30 08:54:40 +0200

    improve refresh token handling and remove debug

--- src/api/login_api.py
+++ src/api/login_api.py
@@ -57,9 +57,15 @@ def index():
 })
 def login():
     data = request.json
+    token_time = int(os.getenv('TOKEN_TIME'))
+    if not token_time:
+        token_time = 15
+    refresh_token_time = int(os.getenv('REFRESH_TOKEN_TIME'))
+    if not refresh_token_time:
+        refresh_token_time = 300
     if data['token'] == os.getenv('ADMIN_TOKEN'):
-        access_token = create_access_token(identity='admin',expires_delta=timedelta(minutes=15))
-        refresh_token = create_refresh_token(identity='admin')
+        access_token = create_access_token(identity='admin',expires_delta=timedelta(minutes=token_time))
+        refresh_token = create_refresh_token(identity='admin',expires_delta=timedelta(minutes=refresh_token_time))
         return jsonify(access_token=access_token, refresh_token=refresh_token), 200
     return jsonify({"msg": "Bad admin token"}), 401
 
@@ -91,5 +97,9 @@ def login():
 @jwt_required(refresh=True)
 def refresh():
     current_user = get_jwt_identity()
-    new_access_token = create_access_token(identity=current_user, expires_delta=timedelta(minutes=15))
+    token_time = int(os.getenv('TOKEN_TIME'))
+    if not token_time:
+        token_time = 15
+
+    new_access_token = create_access_token(identity=current_user, expires_delta=timedelta(minutes=token_time))
     return jsonify(access_token=new_access_token), 200
--- src/database/abstract_database_service.py
+++ src/database/abstract_database_service.py
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 class AbstractDatabaseService(ABC):
     def __init__(self, db_url):
         self.engine = create_engine(db_url)
-        logger.debug("DB URL: " + db_url)
         Base.metadata.create_all(self.engine)
         self.Session = scoped_session(sessionmaker(bind=self.engine))
 
